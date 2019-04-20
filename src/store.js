6e20f7c293f7e84c2ec6bbef3d7a38563fcee2d0
Christian <cpmontebon25@gmail.com>
2019-04-20 21:33:39 +0800

    Add custom reducer for alert component

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import firebase from "firebase";
 import "firebase/firestore";
 import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
+import notifyReducer from "./reducers/notifyReducer";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBns_Yz8WQuNNPdyL0YtGcTvG3jDrMFOPE",
@@ -33,7 +34,8 @@ const createStoreWithFirebase = compose(
 // Add firebase to reducers
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
-  firestore: firestoreReducer // <- needed if using firestore
+  firestore: firestoreReducer,
+  notify: notifyReducer
 });
 
 // Create store with reducers and initial state
