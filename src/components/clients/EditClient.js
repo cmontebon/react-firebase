ed90d78b1d78a8cf00b8fcdcb139836fa920b647
Christian <cpmontebon25@gmail.com>
2019-04-20 19:05:22 +0800

    Add edit client functionality

---
+++ src/components/clients/EditClient.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+
+class EditClient extends Component {
+  constructor() {
+    super();
+
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+    this.balanceInput = React.createRef();
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const clientUpdate = {
+      firstName: this.firstNameInput.current.value,
+      lastName: this.lastNameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value,
+      balance:
+        this.balanceInput.current.value === ""
+          ? 0
+          : this.balanceInput.current.value
+    };
+
+    const { history, firestore } = this.props;
+
+    firestore
+      .update(
+        { collection: "clients", doc: this.props.match.params.id },
+        clientUpdate
+      )
+      .then(() => history.push("/"));
+  };
+
+  render() {
+    const { client } = this.props;
+
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left" /> Back To Dashboard
+              </Link>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-header">Add Client</div>
+            <div className="card-body">
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="firstName"
+                    minLength="2"
+                    required
+                    defaultValue={client.firstName}
+                    ref={this.firstNameInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="lastName"
+                    minLength="2"
+                    required
+                    defaultValue={client.lastName}
+                    ref={this.lastNameInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    defaultValue={client.email}
+                    ref={this.emailInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="phone">Phone</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    minLength="10"
+                    name="phone"
+                    defaultValue={client.phone}
+                    ref={this.phoneInput}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="balance">Balance</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="balance"
+                    defaultValue={client.balance}
+                    ref={this.balanceInput}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
+
+    return (
+      <div>
+        <h3>Edit</h3>
+      </div>
+    );
+  }
+}
+
+EditClient.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
+
+export default compose(
+  firestoreConnect(props => [
+    { collection: "clients", storeAs: "client", doc: props.match.params.id }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(EditClient);
